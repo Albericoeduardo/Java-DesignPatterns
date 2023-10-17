ec2deaaeb9edfdd34079c9f10038a4aca4861c4f
Albérico Eduardo <albericoeduardo202@gmail.com>
2023-10-17 00:20:37 -0300

    Java Design Patterns

---
+++ src/Singleton/SingletonLazyHolder.java
@@ -0,0 +1,14 @@
+package Singleton;
+
+public class SingletonLazyHolder {
+    private static class InstaceHolder {
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+    public SingletonLazyHolder(){
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia(){
+        return InstaceHolder.instancia;
+    }
+}
